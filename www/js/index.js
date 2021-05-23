33e31f7cd2dbafa2b3bd993c95e8197c479f7ac5
Maxim Brochin <primebruh01@gmail.com>
2021-05-23 02:31:03 -0400

    Added request for loading gallery

--- www/js/index.js
+++ www/js/index.js
@@ -3,6 +3,8 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 // When device is ready, listen for events
 function onDeviceReady() {
+    // Load gallery
+    loadGallery();
     // Listen for camera tab click
     $('a[href="#camera"]').on('click', takePicture);
     // Listen for gallery tab click
@@ -11,23 +13,84 @@ function onDeviceReady() {
     $('#cancel').on('click', cancelPicture);
 }
 
+// Loads the gallery from firebase
+function loadGallery() {
+
+    // Refresh gallery
+    const gallery = document.getElementById('gallery');
+    const list = gallery.querySelectorAll('div.card');
+    for(const card of list){
+        card.remove();
+    }
+
+    // List all images
+    storageRef.child('/images').listAll()
+        .then((res) => {
+            res.items.forEach((itemRef) => {
+
+                // Create card element with image
+                const card = document.createElement('div');
+                card.className = 'card shadow my-5 mx-auto';
+                card.style.width = '18rem';
+                card.innerHTML = `
+                    <img src="./img/favicon.ico" class="card-img-top" alt="${itemRef.name}">
+                    <div class="card-body">
+                    <p class="card-text">This location is perfect for a weekend away</p>
+                    </div>`;
+                gallery.appendChild(card);
+                
+                // Get each download URL and update img
+                storageRef.child(`/images/${itemRef.name}`).getDownloadURL()
+                    .then((url) => {
+                        card.querySelector('img').src = url;
+                    })
+                    .catch((err) => {
+                        alert(err)
+                    });
+            });
+
+            // Create default card if no images
+            if(!res.items.length){
+                const card = document.createElement('div');
+                card.className = 'card shadow my-5 mx-auto';
+                card.style.width = '18rem';
+                card.innerHTML = `
+                    <div class="card-body">
+                    <p class="card-text">No pictures have been uploaded.</p>
+                    </div>`;
+                gallery.appendChild(card);
+            }
+        }).catch((err) => {
+            alert(err);
+        });
+}
+
 // Removes picture from preview and toggles gallery section
 function cancelPicture() {
+
     // Remove preview image
     const elem = document.getElementById('imageFile');
     elem.src = '';
+
     // Toggle gallery section
     $('#preview').addClass('d-none');
     $('#success').addClass('d-none');
     $('#gallery').removeClass('d-none');
+    $('#addSpace').removeClass('d-none');
+
+    // Load gallery
+    loadGallery();
 }
 
 // Takes a picture using the user's device
 function takePicture() {
     // Take a picture
     navigator.camera.getPicture((imgUri) => {
+
         // On success, hide gallery and display image for preview
         $('#gallery').addClass('d-none');
+        $('#addSpace').addClass('d-none');
+        $('#success').addClass('d-none');
         $('#preview').removeClass('d-none');
         const elem = document.getElementById('imageFile');
         const name = `data:image/jpeg;base64,${imgUri}`
@@ -35,8 +98,9 @@ function takePicture() {
 
         // Listen for upload click
         $('#upload').on('click', () => {
+
             // Create a reference for image
-            var ref = storageRef.child(window.crypto.getRandomValues(new Uint32Array(10)).toString());
+            var ref = storageRef.child(`/images/${window.crypto.getRandomValues(new Uint32Array(10)).toString()}`);
 
             // Convert URI into file object
             let arr = name.split(','), mime = arr[0].match(/:(.*?);/)[1],
@@ -48,11 +112,9 @@ function takePicture() {
 
             // Store file into database
             ref.put(file).then(() => {
-                // Toggle gallery section
+                // Toggle success message
                 $('#preview').addClass('d-none');
                 $('#success').removeClass('d-none');
-                /*
-                $('#gallery').removeClass('d-none');*/
             });
         });
     }, (err) => {
